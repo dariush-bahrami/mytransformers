080e475812821622eb63ebdd564dfbe218ec6ffa
Dariush Bahrami <dariushbahrami1993@gmail.com>
2023-04-07 11:43:06 +0330

    feat: generalize contexts representation

--- mytransformers/experiment/timeseries/dataset.py
+++ mytransformers/experiment/timeseries/dataset.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import Dict, TypedDict
 
 from torch import Tensor
 from torch.utils.data import Dataset
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 
 class DataDict(TypedDict):
     past_time: Tensor
-    past_context: Tensor
+    past_contexts: Dict[str, Tensor]
     past_target: Tensor
     future_time: Tensor
     future_target: Tensor
@@ -16,13 +16,13 @@ class TimeSeriesTransformerDataset(Dataset):
     def __init__(
         self,
         time: Tensor,
-        context: Tensor,
+        contexts: Dict[str, Tensor],
         target: Tensor,
         past_sequence_length: int,
         future_sequence_length: int,
     ) -> None:
         self.time = time
-        self.context = context
+        self.contexts = contexts
         self.target = target
         self.past_sequence_length = past_sequence_length
         self.future_sequence_length = future_sequence_length
@@ -38,9 +38,14 @@ class TimeSeriesTransformerDataset(Dataset):
         future_idx_start = past_idx_end
         future_idx_end = past_idx_end + self.future_sequence_length
 
+        # Create past context dictionary
+        past_contexts = {}
+        for key, value in self.contexts.items():
+            past_contexts[key] = value[past_idx_start:past_idx_end]
+
         return {
             "past_time": self.time[past_idx_start:past_idx_end],
-            "past_context": self.context[past_idx_start:past_idx_end],
+            "past_contexts": past_contexts,
             "past_target": self.target[past_idx_start:past_idx_end],
             "future_time": self.time[future_idx_start:future_idx_end],
             "future_target": self.target[future_idx_start:future_idx_end],
