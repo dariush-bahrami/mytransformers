f50194f16cf98dd8c1d95a7632eb3bc256bba402
Dariush Bahrami <dariushbahrami1993@gmail.com>
2024-02-17 03:18:10 +0330

    remove!: delete unnecessary wrapper

--- mytransformers/building_blocks/tokenembedding.py
+++
@@ -1,8 +0,0 @@
-from torch import nn
-
-
-class TokenEmbedder(nn.Embedding):
-    def __init__(self, vocabulary_size: int, embedding_dimension: int):
-        super().__init__(vocabulary_size, embedding_dimension)
-        self.vocabulary_size = vocabulary_size
-        self.embedding_dimension = embedding_dimension
